3b07865423a2a433762179282f379e66d8db1535
Andres Hincapie <ahincapie@andress-mbp-2.myfiosgateway.com>
2023-01-31 20:39:32 -0500

    HTML and assets updated

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-let wordsArr = ['Hangman', '']
+//*** Words Object ***/
+let wordsArray = [
+    {word: 'Czech Republic', hint: 'It has the most castles in Europe'},
+    {word: 'Yakisoba', hint: 'It is a Japanese variety of fried noodles'},
+    {word: 'Goodfellas', hint: 'As far back as I can remember, I always wanted to be a gangster'}
+]
+
+class Player{
+    constructor(playerName){
+        this.playerName = playerName
+        this.score = 0
+    }
+}
+
+function getRandomWord(wordsArray) {
+    let randomNumber = Math.floor(Math.random() )
+    let randomWord = wordsArray[randomNumber] 
+    console.log(randomWord)
+
+    return randomWord
+}
+
+let gessingWord = getRandomWord(wordsArray)

5f4d91d72326db6ce406a15bafbab59cc18fb2fd
Andres Hincapie <ahincapie@andress-mbp-2.myfiosgateway.com>
2023-02-03 18:47:56 -0500

    Changes in app.js were made

--- app.js
+++ app.js
@@ -28,8 +28,35 @@ class Player {
   }
 }
 
-//*** Esta funcion permite asignar una palabra aleatoria ***/
-function setTheGame() {
+class Game {
+  constructor(){
+    this.clickedWrong = 0
+    this.rocketDistance = 128
+  }
+  attack(){
+    this.rocketDistance += 68
+    return this.rocketDistance
+  }
+}
+
+//*** Esta funcion permite inicializar el juego para comenzar a jugar ***/
+function startGame() {
+
+  //*** Class game created ***/
+  let game = new Game()
+
+  //** Players are created ***/
+  let playersArray = [];
+  let numberOfPlayers = prompt("Press 1 for one player game. Press 2 for two player game");
+  if (numberOfPlayers == 1) {
+    let player1 = new Player();
+  } else {
+    let player1 = new Player();
+    playersArray.push(player1);
+    
+    let player2 = new Player();
+    playersArray.push(player2);
+  }
 
   //*** Esta funcion retorna un numero aleatorio para obtener del vector la palabra que se debe adivinar ***/
   function getRandomWord(wordsArray) {
@@ -41,7 +68,7 @@ function setTheGame() {
   }
 
   let guessingWord = getRandomWord(wordsArray);
-  const guessWordArray = guessingWord.word;
+  let guessWordArray = guessingWord.word;
   let guessWordAuxArr = []
 
   //*** Este bloque, permite mostrar la palabra a adivinar en la pagina ***/
@@ -64,60 +91,55 @@ function setTheGame() {
     }
   }
 
-  console.log(guessWordAuxArr)
-
-  //** Se crear los jugadores ***/
-  let playersArray = [];
-  let numberOfPlayers = prompt("Press 1 for one player game. Press 2 for two player game");
-  if (numberOfPlayers == 1) {
-    let player1 = new Player();
-  } else {
-    let player1 = new Player();
-    playersArray.push(player1);
-
-    let player2 = new Player();
-    playersArray.push(player2);
-  }
-
   //*** Function for all the letters and adding the event listener */
+  let attemptsWrong = 0
   let selectedLetter = document.querySelectorAll("#keyboard img");
 
   selectedLetter.forEach(imgs => {
     imgs.addEventListener("click", function (evt) {
         evt.preventDefault();
         
+        if(attemptsWrong == 6){
+          console.log('Si funciona el IF statement')
+        }
+
         let i = 0
         let letterFound = false
         while (i < guessWordArray.length) {
           if(evt.target.id == guessWordArray[i].toLowerCase()){
               evt.target.src = "images/correct.png";
               guessWordAuxArr[i] = evt.target.id
-              console.log(guessWordAuxArr)
               letterFound = true
               i++
           } else if(i == guessWordArray.length - 1 && !letterFound){
                 console.log(guessWordAuxArr[i])
                 evt.target.src = "images/wrong.png";
+                let rocketDistance = document.getElementById('rocket')
+                rocketDistance.style.top = game.attack() + 'px'
+                attemptsWrong++
                 i++
             } else {
                 i++
             }
-            console.log(i);
         }
+
         guessWordEl.innerHTML = ""
         guessWordAuxArr.forEach(letter => {
             if(letter == "_"){
                 let img = document.createElement('img')
+                img.setAttribute("href", "#")
                 img.classList.add("alphabet");
                 img.src = "images/blank.png";
                 guessWordEl.appendChild(img);
             } else if(letter == " "){
                 let img = document.createElement('img')
+                img.setAttribute("href", "#")
                 img.classList.add("alphabet");
                 img.src = "images/space.png";
                 guessWordEl.appendChild(img);
             } else {
                 let img = document.createElement('img')
+                img.setAttribute("href", "#")
                 img.classList.add("alphabet");
                 img.src = "images/" + letter + "1.png";
                 guessWordEl.appendChild(img);
@@ -132,11 +154,12 @@ function setTheGame() {
 let startPlay = document.getElementById("startButton");
 startPlay.addEventListener("click", function (evt) {
   evt.preventDefault();
-  setTheGame();
+  // let body = document.querySelector('body')
+  // body.style.backgroundColor = "black"
+  // body.style.opacity = 0.3
+  // console.dir(body)
+  startGame();
 });
-
-//*** Esta funcion permite inicializar el juego para comenzar a jugar ***/
-function startGame() {}
 
 //*** Esta funcion muestra la ayuda para adivinar la palabra. Cada jugar solo tiene 3 oportunidades de usar la ayuda ***/
 function help() {}
